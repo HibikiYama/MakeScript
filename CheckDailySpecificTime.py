d5dc1c0cd7b70770a2387af9aaf8cd008fedaa79
Hibiki <yama@iral.ess.sci.osaka-u.ac.jp>
2024-10-14 23:25:53 -1000

    ver1.5.1

--- CheckDailySpecificTime.py
+++ CheckDailySpecificTime.py
@@ -7,6 +7,8 @@ from appdirs import user_data_dir
 from datetime import datetime, date, time, timezone, timedelta
 import function as fun
 
+print('\nPlease wait a while...')
+
 app_name = 'MakeScript'
 date = datetime.now(timezone.utc)
 year = date.timetuple()[0] - 2000 #*ex) 24 = 2024 - 2000
@@ -35,39 +37,42 @@ with open(stime_file_path, mode='r') as f:
         stime = row_scm[2]
         stime_sub = re.split('_', stime) #*split with under bar
         utc = stime_sub[0]
-        start_tar = stime_sub[1]
-        start_tar_scl = re.split(':', start_tar) #*split with colon
-        start_hour = start_tar_scl[0]
-        start_min = start_tar_scl[1]
-        end_tar = stime_sub[2]
-        end_tar_scl = re.split(':', end_tar) #*split with colon
-        end_hour = end_tar_scl[0]
-        end_min = end_tar_scl[1]
-        date_tar = stime_sub[3]
-        date_tar_ssl = re.split('/', date_tar) #*split with slash
-        day_tar = date_tar_ssl[0]
-        month_tar = date_tar_ssl[1]
-        year_tar = date_tar_ssl[2]
+        if utc == 'UTC':
+            start_tar = stime_sub[1]
+            start_tar_scl = re.split(':', start_tar) #*split with colon
+            start_hour = start_tar_scl[0]
+            start_min = start_tar_scl[1]
+            end_tar = stime_sub[2]
+            end_tar_scl = re.split(':', end_tar) #*split with colon
+            end_hour = end_tar_scl[0]
+            end_min = end_tar_scl[1]
+            date_tar = stime_sub[3]
+            date_tar_ssl = re.split('/', date_tar) #*split with slash
+            day_tar = date_tar_ssl[0]
+            month_tar = date_tar_ssl[1]
+            year_tar = date_tar_ssl[2]
 
-        if int(month) == int(month_tar):
-            if int(day) == int(day_tar):
-                if int(start_hour) > 12:
-                    utc_datetime_start = datetime(int(year_tar), int(month_tar), int(day_tar), int(start_hour), int(start_min))
-                    utc_datetime_end = datetime(int(year_tar), int(month_tar), int(day_tar), int(end_hour), int(end_min))
-                    sast_start = fun.utc2sast(utc_datetime_start)
-                    sast_end = fun.utc2sast(utc_datetime_end)
-                    row_new = script_name + ', ' + block_id + ', ' + str(sast_start.date()) +' '+ str(sast_start.time()) + ' - ' + str(sast_end.date()) +' '+ str(sast_end.time()) + ' (SAST)\n'
-                    text_list.append(row_new)
-                else:
-                    pass
-            elif int(day) == int(day_tar) - 1:
-                if int(start_hour) < 12:
-                    utc_datetime_start = datetime(int(year_tar), int(month_tar), int(day_tar), int(start_hour), int(start_min))
-                    utc_datetime_end = datetime(int(year_tar), int(month_tar), int(day_tar), int(end_hour), int(end_min))
-                    sast_start = fun.utc2sast(utc_datetime_start)
-                    sast_end = fun.utc2sast(utc_datetime_end)
-                    row_new = script_name + ', ' + block_id + ', ' + str(sast_start.date()) +' '+ str(sast_start.time()) + ' - ' + str(sast_end.date()) +' '+ str(sast_end.time()) + ' (SAST)\n'
-                    text_list.append(row_new)
+            if int(month) == int(month_tar):
+                if int(day) == int(day_tar):
+                    if int(start_hour) > 12:
+                        utc_datetime_start = datetime(int(year_tar), int(month_tar), int(day_tar), int(start_hour), int(start_min))
+                        utc_datetime_end = datetime(int(year_tar), int(month_tar), int(day_tar), int(end_hour), int(end_min))
+                        sast_start = fun.utc2sast(utc_datetime_start)
+                        sast_end = fun.utc2sast(utc_datetime_end)
+                        row_new = script_name + ', ' + block_id + ', ' + str(sast_start.date()) +' '+ str(sast_start.time()) + ' - ' + str(sast_end.date()) +' '+ str(sast_end.time()) + ' (SAST)\n'
+                        text_list.append(row_new)
+                    else:
+                        pass
+                elif int(day) == int(day_tar) - 1:
+                    if int(start_hour) < 12:
+                        utc_datetime_start = datetime(int(year_tar), int(month_tar), int(day_tar), int(start_hour), int(start_min))
+                        utc_datetime_end = datetime(int(year_tar), int(month_tar), int(day_tar), int(end_hour), int(end_min))
+                        sast_start = fun.utc2sast(utc_datetime_start)
+                        sast_end = fun.utc2sast(utc_datetime_end)
+                        row_new = script_name + ', ' + block_id + ', ' + str(sast_start.date()) +' '+ str(sast_start.time()) + ' - ' + str(sast_end.date()) +' '+ str(sast_end.time()) + ' (SAST)\n'
+                        text_list.append(row_new)
+                    else:
+                        pass
                 else:
                     pass
             else:
